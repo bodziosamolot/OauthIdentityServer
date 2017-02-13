27c5ccd491514153928b902502f96d289ee08419
jogieglo@future-processing.com <jogieglo@future-processing.com>
2017-02-13 10:14:00 +0100

    Introduction of AspNet Core Auth Server

---
+++ AuthenticationServer/AuthServer/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IdentityServer4.Models;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AuthServer
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _environment = env;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var cert = new X509Certificate2(Path.Combine(_environment.ContentRootPath, "MyKey.pfx"), "1qaz@WSX");
+
+            services.AddIdentityServer()
+                .AddSigningCredential(cert)
+                .AddInMemoryClients(new List<Client>())
+                .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddTestUsers(new List<IdentityServer4.Test.TestUser>())
+                .AddTemporarySigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
